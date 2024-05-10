96c46c81effabd7e57fbf131125f5b7b5209bae5
Νεφέλη Κωνσταντάτου <nephelekonstantatou@gmail.com>
2024-05-10 04:36:40 +0300

    Eww: Can now scroll to change keyboard layout

--- .config/eww/scripts/volume-adjust.sh
+++ .config/eww/scripts/volume-adjust.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 if [ "$1" = "up" ]; then
-    # Increase the volume by 0.1
     wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%+
 elif [ "$1" = "down" ]; then
-    # Decrease the volume by 0.1
     wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%-
 else
     exit -1
