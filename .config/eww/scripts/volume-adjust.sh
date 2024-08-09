9cfa87dda7e1ad54950cb2debfc4144852b60b4d
Νεφέλη Κωνσταντάτου <nephelekonstantatou@gmail.com>
2024-08-09 07:21:38 +0300

    Additions Updated audio management (eww) Added resizing (hyprland) Added command line scrolling to kitty

--- .config/eww/scripts/volume-adjust.sh
+++ .config/eww/scripts/volume-adjust.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+wp_devices() {
+    wpctl status | awk '/Sinks:/{flag=1; next} /Sources:/{flag=0} flag' | awk -F'.' '{print $1}' | grep -o '[0-9]\+'
+}
+
 if [ "$1" = "up" ]; then
-    wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%+
+    wpctl set-volume @DEFAULT_AUDIO_SINK@ 4%+
 elif [ "$1" = "down" ]; then
-    wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%-
+    wpctl set-volume @DEFAULT_AUDIO_SINK@ 4%-
 else
     exit -1
 fi
+
+volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | sed 's/[^0-9.]//g')
+wp_devices | while IFS= read -r line; do
+    wpctl set-volume "$line" $volume
+done
